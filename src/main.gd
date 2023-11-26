9c9add1fbc390eae2accd82dee3efca02ded37f3
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-26 12:09:22 -0500

    Main scene and game scene

---
+++ src/main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var initial_scene: PackedScene
+
+
+func _ready() -> void:
+	var scene: Scene = initial_scene.instantiate()
+	_switch_scene(scene)
+
+
+func _switch_scene(new_scene: Scene) -> void:
+	if get_child_count() > 0:
+		assert(get_child_count() == 1)
+		remove_child(get_child(0))
+	add_child(new_scene)
+	new_scene.switch_scene.connect(_switch_scene)
---
+++ src/scene.gd
@@ -0,0 +1,4 @@
+class_name Scene
+extends Node
+
+signal switch_scene(new_scene: Scene)
