eb3cc1e5f0b9ffd1e55cb9c172bc9140f8b65e2a
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-26 12:13:43 -0500

    Extra asserts

--- src/main.gd
+++ src/main.gd
@@ -12,5 +12,7 @@ func _switch_scene(new_scene: Scene) -> void:
 	if get_child_count() > 0:
 		assert(get_child_count() == 1)
 		remove_child(get_child(0))
+	assert(get_child_count() == 0)
 	add_child(new_scene)
+	@warning_ignore("return_value_discarded")
 	new_scene.switch_scene.connect(_switch_scene)
