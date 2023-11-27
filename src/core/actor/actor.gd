a703bd139af5c6581be8f744bf72699f2514ac9d
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-26 19:00:40 -0500

    Actor sprite and definition properties

--- src/core/actor/actor.gd
+++ src/core/actor/actor.gd
@@ -2,16 +2,29 @@
 class_name Actor
 extends TileObject
 
+@export var sprite: Texture2D:
+	get:
+		var result: Texture2D = null
+		if _sprite:
+			result = _sprite.texture
+		return result
+	set(value):
+		if _sprite:
+			_sprite.texture = value
+
+
+@export var definition: ActorDefinition
+
 @onready var _sprite: ActorSprite = $ActorSprite
 
 
-func _on_tile_size_changed(old_size: Vector2i) -> void:
+func _on_tile_size_changed(_old_size: Vector2i) -> void:
 	_sprite.tile_size = tile_size
 
 
-func _on_origin_cell_changed(old_cell: Vector2i) -> void:
+func _on_origin_cell_changed(_old_cell: Vector2i) -> void:
 	pass # Replace with function body.
 
 
-func _on_cell_size_changed(old_size: Vector2i) -> void:
+func _on_cell_size_changed(_old_size: Vector2i) -> void:
 	_sprite.cell_size = cell_size
