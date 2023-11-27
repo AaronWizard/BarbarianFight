acf68913554cda4113945009b60d6f2c4bf0f928
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-26 19:00:27 -0500

    ActorSprite.texture property

--- src/core/actor/actor_sprite.gd
+++ src/core/actor/actor_sprite.gd
@@ -2,15 +2,27 @@
 class_name ActorSprite
 extends TileObject
 
+
+@export var texture: Texture2D:
+	get:
+		var result: Texture2D = null
+		if _sprite:
+			result = _sprite.texture
+		return result
+	set(value):
+		if _sprite:
+			_sprite.texture = value
+
+
 @onready var _sprite_origin: Node2D = $SpriteOrigin
 @onready var _sprite: Sprite2D = $SpriteOrigin/Sprite
 
 
-func _on_tile_size_changed(old_size: Vector2i) -> void:
+func _on_tile_size_changed(_old_size: Vector2i) -> void:
 	_update_sprite_origin()
 
 
-func _on_cell_size_changed(old_size: Vector2i) -> void:
+func _on_cell_size_changed(_old_size: Vector2i) -> void:
 	_update_sprite_origin()
 
 
