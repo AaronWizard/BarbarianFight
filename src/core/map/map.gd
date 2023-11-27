c4fa9ca5b4b823883e0509c2794f929ff70e4781
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-27 18:54:49 -0500

    Map.add_actor

--- src/core/map/map.gd
+++ src/core/map/map.gd
@@ -10,3 +10,10 @@ var actors: ActorMap:
 var markers: MapMarkers:
 	get:
 		return $MapMarkers as MapMarkers
+
+
+func add_actor(actor: Actor, cell: Vector2i) -> void:
+	assert(actor.get_parent() == null)
+	assert(actors.actor_can_enter_cell(actor, cell))
+	actor.origin_cell = cell
+	actors.add_child(actor)
