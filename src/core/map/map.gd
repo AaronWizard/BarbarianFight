5b823553b839142c9ca1d0133a01aecb5a20ce94
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-27 11:50:55 -0500

    Started MapMarkers class

--- src/core/map/map.gd
+++ src/core/map/map.gd
@@ -4,4 +4,9 @@ extends Node2D
 
 var actors: ActorMap:
 	get:
-		return $ActorMap
+		return $ActorMap as ActorMap
+
+
+var markers: MapMarkers:
+	get:
+		return $MapMarkers as MapMarkers
---
+++ src/core/map/map_markers.gd
@@ -0,0 +1,2 @@
+class_name MapMarkers
+extends Node2D
