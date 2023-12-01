52cfebdad13678c2791f50480f662dbb6a1a6744
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-11-30 23:21:08 -0500

    Use Brain node in Actor scene.  Villain has AI.

--- src/core/actor/actor.gd
+++ src/core/actor/actor.gd
@@ -7,11 +7,29 @@ extends TileObject
 ## An actor. An actor has stats and can take turns.
 
 @export var definition: ActorDefinition
+## The scene for the actor's [Brain].
+@export var brain_scene: PackedScene
 
 @onready var _sprite := $ActorSprite as ActorSprite
 @onready var _turn_taker := $TurnTaker as TurnTaker
 
+var _brain: Brain
+
 var _turn_clock: TurnClock
+
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		if brain_scene:
+			var brain := brain_scene.instantiate() as Brain
+			set_brain(brain)
+
+
+## Sets the actor's brain. The brain is added to the actor as a child node.
+func set_brain(brain: Brain) -> void:
+	_brain = brain
+	add_child(_brain)
+	_brain.set_actor(self)
 
 
 ## Sets the actor's turn clock.
@@ -34,8 +52,16 @@ func _cell_size_changed(_old_size: Vector2i) -> void:
 
 
 func _on_turn_taker_turn_started() -> void:
-	print("%s starting turn [%s]" % [name, _turn_taker])
-	await get_tree().create_timer(0.5).timeout
-	print("\tTaking action [delay = %d]" \
-			% TurnConstants.action_delay(TurnConstants.ActionSpeed.MEDIUM))
-	_turn_taker.end_turn(TurnConstants.ActionSpeed.MEDIUM)
+	var action_speed := TurnConstants.ACTION_WAIT_SPEED # Null action is wait
+
+	print("Actor: %s starting turn" % name,)
+	if _brain:
+		@warning_ignore("redundant_await")
+		var action := await _brain.get_action()
+		if action:
+			action_speed = action.get_action_speed()
+			@warning_ignore("redundant_await")
+			await action.run()
+	print("Actor: %s finished turn" % name)
+
+	_turn_taker.end_turn(action_speed)
