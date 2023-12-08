5020031ee55ec7bc67a8c752d43f86e8d05bd1b4
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-12-07 19:07:47 -0500

    Game listens for Actor.player_turn_started

--- src/core/game/game.gd
+++ src/core/game/game.gd
@@ -13,10 +13,16 @@ var _current_map: Map
 
 
 func _ready() -> void:
-	_player = player_actor_scene.instantiate() as Actor
+	_init_player()
 	_load_initial_map()
 
 	_turn_clock.run()
+
+
+func _init_player() -> void:
+	_player = player_actor_scene.instantiate() as Actor
+	@warning_ignore("return_value_discarded")
+	_player.player_turn_started.connect(_on_player_turn_started)
 
 
 func _load_initial_map() -> void:
@@ -32,3 +38,7 @@ func _load_map(map: Map, start_cell: Vector2i) -> void:
 
 	_current_map.set_turn_clock(_turn_clock)
 	_current_map.add_actor(_player, start_cell)
+
+
+func _on_player_turn_started() -> void:
+	print("player turn started")
