d4ff58358d112a24e1e13652a0aac1fa3e524496
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-12-09 13:57:39 -0500

    Player actor blocked by obstacles

--- src/core/control/turnaction/bump_action.gd
+++ src/core/control/turnaction/bump_action.gd
@@ -15,7 +15,18 @@ var _target_cell: Vector2i
 func _init(target_actor: Actor, target_cell: Vector2i) -> void:
 	_target_actor = target_actor
 	_target_cell = target_cell
+	assert(BumpAction.is_possible(_target_actor, _target_cell))
 
 
 func run() -> void:
 	_target_actor.origin_cell = _target_cell
+
+
+static func is_possible(target_actor: Actor, target_cell: Vector2i) -> bool:
+	var result := _can_move_to_target(target_actor, target_cell)
+	return result
+
+
+static func _can_move_to_target(target_actor: Actor, target_cell: Vector2i) \
+		-> bool:
+	return target_actor.map.actor_can_enter_cell(target_actor, target_cell)
--- src/core/ui/player_input.gd
+++ src/core/ui/player_input.gd
@@ -20,7 +20,7 @@ func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_released("wait"):
 		action_chosen.emit(null)
 	else:
-		var action := _try_walk()
+		var action := _try_bump()
 		if action:
 			action_chosen.emit(action)
 
@@ -29,13 +29,14 @@ func set_player_actor(actor: Actor) -> void:
 	_player_actor = actor
 
 
-func _try_walk() -> TurnAction:
+func _try_bump() -> TurnAction:
 	var result: TurnAction = null
 
 	var direction := Vector2i(Input.get_vector(
 			"step_west", "step_east", "step_north", "step_south"))
 	if direction.length_squared() == 1:
 		var target_cell := _player_actor.origin_cell + direction
-		result = BumpAction.new(_player_actor, target_cell)
+		if BumpAction.is_possible(_player_actor, target_cell):
+			result = BumpAction.new(_player_actor, target_cell)
 
 	return result
