f65e0b7279481de20b92e2ce45896b98fbfcabf4
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-12-09 19:58:17 -0500

    Starting ActorSprite animations

--- src/core/actor/actor_sprite.gd
+++ src/core/actor/actor_sprite.gd
@@ -2,7 +2,24 @@
 class_name ActorSprite
 extends TileObject
 
+
+## The offset direction of the sprite. Measured in tiles.
+@export var sprite_offset_vector := Vector2.ZERO:
+	set(value):
+		sprite_offset_vector = value
+		if _sprite:
+			_set_sprite_offset()
+
+
+@export var sprite_offset_distance := 0.0:
+	set(value):
+		sprite_offset_distance = value
+		if _sprite:
+			_set_sprite_offset()
+
+
 @onready var _sprite_origin := $SpriteOrigin as Node2D
+@onready var _sprite := $SpriteOrigin/Sprite as Sprite2D
 
 
 func _tile_size_changed(_old_size: Vector2i) -> void:
@@ -18,3 +35,7 @@ func _update_sprite_origin() -> void:
 		var center := Vector2(cell_size * tile_size) / 2.0
 		center.y *= -1
 		_sprite_origin.position = center
+
+
+func _set_sprite_offset() -> void:
+	_sprite.position = sprite_offset_vector * sprite_offset_distance * Vector2(tile_size)
