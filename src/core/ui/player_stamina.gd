a2dbdd1276ce1f584c482b65e582939c8d74823d
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-12-28 19:00:02 -0500

    Tweaked stamina bar. Removed stamina bar text.

--- src/core/ui/player_stamina.gd
+++ src/core/ui/player_stamina.gd
@@ -10,8 +10,6 @@ var max_stamina: int:
 var current_stamina: int:
 	set(value):
 		_stamina_bar.value = value
-		_stamina_text.text = str(value)
 
 
 @onready var _stamina_bar := $HBoxContainer/StaminaBar as Range
-@onready var _stamina_text := $StaminaText as Label
