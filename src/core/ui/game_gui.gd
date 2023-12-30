9c290d2b64356d36d605c9d91b20e0a0fe6ee031
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2023-12-30 16:42:53 -0500

    Functions for showing and hiding boss bar

--- src/core/ui/game_gui.gd
+++ src/core/ui/game_gui.gd
@@ -7,3 +7,12 @@ extends CanvasLayer
 
 func set_player_actor(actor: Actor) -> void:
 	_player_stamina.set_player(actor)
+
+
+func show_boss_bar(boss: Actor) -> void:
+	_boss_stamina.show_boss(boss)
+	_boss_stamina.visible = true
+
+
+func hide_boss_bar() -> void:
+	_boss_stamina.visible = false
