ce881c4ade821b71e75b6fbbb8949ad45e4d504f
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-01-03 19:58:32 -0500

    Main > game_scene -> initial_scene

--- src/main.gd
+++ src/main.gd
@@ -1,19 +1,18 @@
 extends Node
 
-
-
-@export var game_scene: PackedScene
+@export var initial_scene: PackedScene
 
 
 func _ready() -> void:
-	var game: Game = game_scene.instantiate()
-	_switch_scene(game)
+	var scene: Scene = initial_scene.instantiate()
+	_switch_scene(scene)
 
 
 func _switch_scene(new_scene: Scene) -> void:
 	if get_child_count() > 0:
 		assert(get_child_count() == 1)
 		remove_child(get_child(0))
+
 	assert(get_child_count() == 0)
 	add_child(new_scene)
 	@warning_ignore("return_value_discarded")
