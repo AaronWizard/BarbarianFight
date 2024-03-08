024271f07aa32a07ee497837a885ada70b2353d4
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-03-07 23:19:58 -0500

    TileGeometry
    And some tweaks to TileObject

---
+++ src/core/tile_geometry.gd
@@ -0,0 +1,108 @@
+class_name TileGeometry
+
+## A set of geometry methods related to tiles.
+
+
+## The manhattan distance between two tile cells (number of cells needed to move
+## from [param start] to [param end] while only moving in four cardinal
+## directions).
+static func manhattan_distance(start: Vector2i, end: Vector2i) -> int:
+	var diff := (end - start).abs()
+	return int(diff.x + diff.y)
+
+
+## The cells covered by [param rect].
+static func cells_in_rect(rect: Rect2i) -> Array[Vector2i]:
+	var result: Array[Vector2i] = []
+
+	for x in range(rect.position.x, rect.end.x):
+		for y in range(rect.position.y, rect.end.y):
+			result.append(Vector2i(x, y))
+
+	return result
+
+
+## The cells surrounding [param source_rect] where each cell is between
+## [param min_dist] and [max_dist] cells in manhattan distances to the closest
+## cell covered by [param source_rect].[br]
+## A [param min_dist] of 1 includes the cells adjacent to [param source_rect]. A
+## [param min_dist] of 0 includes the cells covered by [param source_rect].
+static func cells_in_range(source_rect: Rect2i, min_dist: int, max_dist: int,
+		include_diagonals := true) -> Array[Vector2i]:
+	assert(source_rect.size.x > 0)
+	assert(source_rect.size.y > 0)
+	assert(min_dist >= 0)
+	assert(max_dist >= 1)
+	assert(max_dist >= min_dist)
+
+	var result: Array[Vector2i] = []
+
+	var range_length := max_dist - maxi(min_dist, 1) + 1
+
+	var range_north := Rect2i(
+		source_rect.position + Vector2i(0, -max_dist),
+		Vector2i(source_rect.size.x, range_length)
+	)
+	var range_south := Rect2i(
+		source_rect.position \
+				+ Vector2i(0, source_rect.size.y + maxi(min_dist, 1) - 1),
+		Vector2i(source_rect.size.x, range_length)
+	)
+	var range_west := Rect2i(
+		source_rect.position + Vector2i(-max_dist, 0),
+		Vector2i(range_length, source_rect.size.y)
+	)
+	var range_east := Rect2i(
+		source_rect.position \
+				+ Vector2i(source_rect.size.x + maxi(min_dist, 1) - 1, 0),
+		Vector2i(range_length, source_rect.size.y)
+	)
+
+	if min_dist == 0:
+		result.append_array(cells_in_rect(source_rect))
+
+	result.append_array(cells_in_rect(range_north))
+	result.append_array(cells_in_rect(range_east))
+	result.append_array(cells_in_rect(range_south))
+	result.append_array(cells_in_rect(range_west))
+
+	if include_diagonals and max_dist > 1:
+		var corner_range_size := Vector2i(max_dist, max_dist)
+
+		var corner_nw := source_rect.position
+		var range_pos_nw := corner_nw - corner_range_size
+
+		var corner_ne := Vector2i(source_rect.end.x - 1, source_rect.position.y)
+		var range_pos_ne := corner_ne + Vector2i(1, -max_dist)
+
+		var corner_se := source_rect.end - Vector2i.ONE
+		var range_pos_se := corner_se + Vector2i.ONE
+
+		var corner_sw := Vector2i(source_rect.position.x, source_rect.end.y - 1)
+		var range_pos_sw := corner_sw + Vector2i(-max_dist, 1)
+
+		for x in range(0, max_dist):
+			for y in range(0, max_dist):
+				var cell := Vector2i(x, y)
+
+				var cell_ne := cell + range_pos_ne
+				var dist_ne := manhattan_distance(corner_ne, cell_ne)
+				if (dist_ne >= min_dist) and (dist_ne <= max_dist):
+					result.append(cell_ne)
+
+				var cell_nw := cell + range_pos_nw
+				var dist_nw := manhattan_distance(corner_nw, cell_nw)
+				if (dist_nw >= min_dist) and (dist_nw <= max_dist):
+					result.append(cell_nw)
+
+				var cell_sw := cell + range_pos_sw
+				var dist_sw := manhattan_distance(corner_sw, cell_sw)
+				if (dist_sw >= min_dist) and (dist_sw <= max_dist):
+					result.append(cell_sw)
+
+				var cell_se := cell + range_pos_se
+				var dist_se := manhattan_distance(corner_se, cell_se)
+				if (dist_se >= min_dist) and (dist_se <= max_dist):
+					result.append(cell_se)
+
+	return result
--- src/core/tile_object.gd
+++ src/core/tile_object.gd
@@ -65,46 +65,49 @@ extends Node2D
 		queue_redraw()
 
 
+@export var show_grid_in_game := false:
+	set(value):
+		show_grid_in_game = value
+		queue_redraw()
+
+
 ## The cells currently covered by the tile object.
 var covered_cells: Array[Vector2i]:
 	get:
 		return covered_cells_at_cell(origin_cell)
 
 
+## A rect covering the tile object's cells.
+var rect: Rect2i:
+	get:
+		return Rect2i(_top_left_cell(origin_cell), cell_size)
+
+
 func _draw() -> void:
-	if Engine.is_editor_hint():
+	if Engine.is_editor_hint() or show_grid_in_game:
 		for x in range(cell_size.x + 1):
 			var line_x := x * tile_size.x
 			var line_y_end := -cell_size.y * tile_size.y
 			draw_line(Vector2(line_x, 0), Vector2(line_x, line_y_end),
-					grid_colour, 0.25)
+					grid_colour, 1)
 		for y in range(cell_size.y + 1):
 			var line_x_end := cell_size.x * tile_size.x
 			var line_y := (y - cell_size.y) * tile_size.y
 			draw_line(Vector2(0, line_y), Vector2(line_x_end, line_y),
-					grid_colour, 0.25)
+					grid_colour, 1)
 
 		var origin_rect := Rect2(Vector2i.UP * tile_size.y, tile_size)
-		draw_rect(origin_rect, origin_colour, false, 0.5)
+		draw_rect(origin_rect, origin_colour, false, 1)
 
 
 ## Returns true if the tile object covers [param cell], false otherwise.
 func covers_cell(cell: Vector2i) -> bool:
-	var rect := Rect2i(_top_left_cell(origin_cell), cell_size)
 	return rect.has_point(cell)
 
 
 ## The cells the tile object would cover if its origin_cell was [param cell].
 func covered_cells_at_cell(cell: Vector2i) -> Array[Vector2i]:
-	var result: Array[Vector2i] = []
-
-	var top_left := _top_left_cell(cell)
-	for x in range(cell_size.x):
-		for y in range(cell_size.y):
-			var covered_cell := Vector2i(x, y) + top_left
-			result.append(covered_cell)
-
-	return result
+	return TileGeometry.cells_in_rect(Rect2i(_top_left_cell(cell), cell_size))
 
 
 ## Called after tile_size is changed. Can be overriden.
