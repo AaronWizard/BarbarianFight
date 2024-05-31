89ee0439456c197720ded733b2011be98ba273ab
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-05-30 23:53:41 -0400

    AreaTargetRange.source_is_square

--- src/core/abilities/targetrange/area_target_range.gd
+++ src/core/abilities/targetrange/area_target_range.gd
@@ -18,9 +18,17 @@ extends TargetRange
 ## The maximum distance is ([member range_start_dist] + [member range_extend]).
 @export var range_extend := 0
 
+## If true, the source is a square with the source cell at its bottom-left
+## corner (see [TileObject]). If false, the source is a single cell.[br][br]
+## Useful for actors larger than one cell to differentiate movement abilities
+## (that only need the actor's origin cell) and attack actions (that would need
+## the actor's whole square to get the right ranges).
+@export var source_is_square := true
 
 func _get_full_range(source_cell: Vector2i, source_size: int) \
 		-> Array[Vector2i]:
-	var source_rect := TileObject.object_rect(source_cell, source_size)
+	var source_rect := Rect2i(source_cell, Vector2i.ONE)
+	if source_is_square:
+		source_rect = TileObject.object_rect(source_cell, source_size)
 	return TileGeometry.cells_in_range(
 			source_rect, range_start_dist, range_extend)
