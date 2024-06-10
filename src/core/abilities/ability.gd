d647ad86bd7b8812fa2a24be89edcc69077b7037
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-06-10 11:45:42 -0400

    Ability name and get_target_range

--- src/core/abilities/ability.gd
+++ src/core/abilities/ability.gd
@@ -3,10 +3,19 @@ extends Resource
 
 ## Class for an active ability an actor can perform.
 
+@export var name := ""
+
 ## The ability's target range.
 @export var target_range: TargetRange
 ## The ability's effect.
 @export var effect: AbilityEffect
+
+
+func get_target_range(source_actor: Actor) -> TargetRangeData:
+	return target_range.get_target_range(
+		source_actor.origin_cell, source_actor.cell_size,
+		source_actor, source_actor.map
+	)
 
 
 ## Performs the ability at [param target] for [param source_actor].[br]
