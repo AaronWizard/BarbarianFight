d88bc22edd1c72fc9390f0c281d7efe3be79d238
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-07-03 17:18:46 -0400

    Redo target range border

--- src/core/scenes/game/ui/target_display/map_target_range.gd
+++ src/core/scenes/game/ui/target_display/map_target_range.gd
@@ -3,13 +3,11 @@ extends TileMap
 
 const _LAYER_TARGET_RANGE := 0
 
-const _TILE_SOURCE_TARGET := 0
-const _TILE_COORDS_TARGET := Vector2i(0, 0)
+const _TARGET_RANGE_TERRAIN_SET := 0
+const _TARGET_RANGE_TERRAIN := 0
+
 
 func set_target_range(target_range: Array[Vector2i]) -> void:
 	clear()
-	for cell in target_range:
-		set_cell(
-			_LAYER_TARGET_RANGE, cell,
-			_TILE_SOURCE_TARGET, _TILE_COORDS_TARGET
-		)
+	set_cells_terrain_connect(_LAYER_TARGET_RANGE, target_range,
+			_TARGET_RANGE_TERRAIN_SET, _TARGET_RANGE_TERRAIN)
