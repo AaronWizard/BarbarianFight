80deac8c883cdcefd6b8310247d87ed749935072
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-07-25 23:00:55 -0400

    Started shove ability

--- src/core/scenes/game/ui/player_input.gd
+++ src/core/scenes/game/ui/player_input.gd
@@ -15,14 +15,18 @@ enum State {
 	## Player will move or attack.
 	BUMP,
 	## Player will dash.
-	DASH
+	DASH,
+	## Player will shove.
+	SHOVE
 }
 
-@export var player_dash_ability_index := 0
+@export var player_ability_index_dash := 0
+@export var player_ability_index_shove := 1
 
 var _player_actor: Actor
 var _current_state := State.BUMP
 
+var _current_ability: Ability = null
 var _player_target_tracker := PlayerTargetTracker.new()
 
 
@@ -32,12 +36,13 @@ func _ready() -> void:
 
 func _unhandled_input(_event: InputEvent) -> void:
 	_check_state()
+	_update_current_ability()
 
 	match _current_state:
 		State.BUMP:
 			_state_bump()
-		State.DASH:
-			_state_dash()
+		State.DASH, State.SHOVE:
+			_ability_input()
 
 
 func set_player_actor(actor: Actor) -> void:
@@ -51,15 +56,28 @@ func start_turn() -> void:
 
 func _end_turn(action: TurnAction) -> void:
 	set_process_unhandled_input(false)
-	_clear_dash()
+	_clear_ability()
 	action_chosen.emit(action)
 
 
 func _check_state() -> void:
-	if Input.is_action_just_pressed("dash"):
-		_show_dash()
-	elif Input.is_action_just_released("dash"):
-		_clear_dash()
+	if Input.is_action_pressed("dash") and not Input.is_action_pressed("shove"):
+		_current_state = State.DASH
+	elif Input.is_action_pressed("shove") \
+			and not Input.is_action_pressed("dash"):
+		_current_state = State.SHOVE
+	else:
+		_current_state = State.BUMP
+
+
+func _update_current_ability() -> void:
+	var new_ability := _get_current_ability()
+	if new_ability and (_current_ability != new_ability):
+		_current_ability = new_ability
+		_show_ability()
+	elif not new_ability:
+		_current_ability = null
+		_clear_ability()
 
 
 func _state_bump() -> void:
@@ -72,11 +90,11 @@ func _state_bump() -> void:
 			_end_turn(action)
 
 
-func _state_dash() -> void:
+func _ability_input() -> void:
 	if Input.is_action_just_released("wait"):
 		var action := AbilityAction.new(
 			_player_actor, _player_target_tracker.target_cell,
-			_get_dash_ability()
+			_current_ability
 		)
 		_end_turn(action)
 	else:
@@ -86,8 +104,8 @@ func _state_dash() -> void:
 			target_cell_changed.emit(_player_target_tracker.target_cell)
 
 
-func _show_dash() -> void:
-	var target_range_data := _get_dash_ability().get_target_range(_player_actor)
+func _show_ability() -> void:
+	var target_range_data := _current_ability.get_target_range(_player_actor)
 
 	_player_target_tracker.set_target_range(target_range_data.valid_targets)
 	show_target_range.emit(
@@ -98,15 +116,23 @@ func _show_dash() -> void:
 	_current_state = State.DASH
 
 
-func _clear_dash() -> void:
+func _clear_ability() -> void:
 	_player_target_tracker.clear()
 	hide_target_range.emit()
 
 	_current_state = State.BUMP
 
 
-func _get_dash_ability() -> Ability:
-	return _player_actor.abilities[player_dash_ability_index]
+func _get_current_ability() -> Ability:
+	var result: Ability = null
+
+	match _current_state:
+		State.DASH:
+			result = _player_actor.abilities[player_ability_index_dash]
+		State.SHOVE:
+			result = _player_actor.abilities[player_ability_index_shove]
+
+	return result
 
 
 func _try_bump() -> TurnAction:
