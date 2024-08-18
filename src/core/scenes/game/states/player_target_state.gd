323601133d248597bdeece7459d2375957d9ae57
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-08-17 20:39:48 -0400

    Use state machine for player control

---
+++ src/core/scenes/game/states/player_target_state.gd
@@ -0,0 +1,63 @@
+class_name PlayerTargetState
+extends State
+
+@export var action_state: PlayerActionState
+@export var movement_state: PlayerMovementState
+
+@export var target_display: TargetDisplay
+
+var _player_target_tracker := PlayerTargetTracker.new()
+
+var _player: Actor
+var _ability: Ability
+var _input_code: String
+
+
+func enter(data := {}) -> void:
+	_player = data.player
+	_ability = data.ability
+	_input_code = data.input_code
+
+	@warning_ignore("unsafe_cast")
+	var targeting_data := data.targeting_data as TargetingData
+	_player_target_tracker.set_targets(targeting_data.targets)
+	target_display.show_range(targeting_data)
+
+
+func exit() -> void:
+	target_display.clear()
+	_player_target_tracker.clear()
+
+	_player = null
+	_ability = null
+	_input_code = ""
+
+
+func handle_input(_event: InputEvent) -> void:
+	if Input.is_action_just_released(_input_code):
+		request_state_change(movement_state, { player = _player })
+	elif _player_target_tracker.has_targets:
+		if Input.is_action_just_released("wait"):
+			var action := AbilityAction.new(
+				_player, _player_target_tracker.target.position, _ability
+			)
+			_end_turn(action)
+		else:
+			_try_move_target()
+
+
+func _try_move_target() -> void:
+	var direction := _get_direction_input()
+	if direction.length_squared() == 1:
+		_player_target_tracker.move_target(direction)
+		target_display.set_target(_player_target_tracker.target)
+
+
+func _get_direction_input() -> Vector2i:
+	return Vector2i(Input.get_vector(
+			"step_west", "step_east", "step_north", "step_south"))
+
+
+func _end_turn(action: TurnAction) -> void:
+	var data := { action = action }
+	request_state_change(action_state, data)
--- src/core/statemachine/state_machine.gd
+++ src/core/statemachine/state_machine.gd
@@ -1,15 +1,10 @@
-@tool
 class_name StateMachine
 extends Node
 
 ## A state machine with [State] objects as children.
 
 
-@export var start_state: State:
-	set(value):
-		start_state = value
-		update_configuration_warnings()
-
+@export var start_state: State
 
 var _current_state: State
 
@@ -24,15 +19,6 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	_current_state.process(delta)
-
-
-func _get_configuration_warnings() -> PackedStringArray:
-	var result: PackedStringArray = []
-	if start_state == null:
-		result = ["start_state is not set"]
-	elif start_state.get_parent() != self:
-		result = ["start_state is not a child of this state machine"]
-	return result
 
 
 func change_state(new_state: State, data := {}) -> void:
