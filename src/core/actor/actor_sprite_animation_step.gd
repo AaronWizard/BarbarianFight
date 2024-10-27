570ff86d60de2b9115e29d3c97558ad3c93c27e9
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-10-27 09:48:47 -0400

    Named actor sprite animation steps

--- src/core/actor/actor_sprite_animation_step.gd
+++ src/core/actor/actor_sprite_animation_step.gd
@@ -7,6 +7,7 @@ extends Resource
 ## move the sprite to, which is relative to the actor's origin cell and a
 ## target cell.
 
+
 ## Sets whether the sprite moves at a constant speed or if the animation runs
 ## for a constant duration.
 enum TimeType {
@@ -20,18 +21,21 @@ enum TimeType {
 	DURATION
 }
 
+## The name of the step. This is the return value of [method animate].
+@export var step_name: String
+
 ## Positions the sprite at the interpolated point between the actor's origin
 ## cell and the animation's target cell. e.g. If this value is 0.5, the sprite
 ## is moved to halfway between the actor's origin cell and the target cell. Can
 ## be negative.[br]
-## Combined with [member ActorSpriteAnimationStep.cell_distance].
+## Combined with [member cell_distance].
 @export var interpolated_distance := 0.0
 
 ## Positions the sprite along the vector from the actor's origin cell to the
 ## animation's target cell, at this value away from the origin cell. e.g. If
 ## this value is 2, the sprite is moved to a position along the vector 2 cells
 ## away from the actor's origin cell. Measured in cells. Can be negative.[br]
-## Combined with [member ActorSpriteAnimationStep.interpolated_distance].
+## Combined with [member interpolated_distance].
 @export var cell_distance := 0.0
 
 ## The amount of time it takes to move the sprite from its current position to
@@ -50,11 +54,11 @@ enum TimeType {
 @export var ease_type := Tween.EASE_IN_OUT
 
 
-## Animates an actor's sprite.[br]
+## Animates an actor's sprite. Returns [member step_name] when finished.[br]
 ## [param target_cell] is relative to the actor's origin cell. [param tile_size]
 ## is in pixels.
 func animate(sprite: Sprite2D, target_cell: Vector2, tile_size: Vector2i) \
-		-> void:
+		-> String:
 	var final_cell_position := _get_final_cell_position(target_cell)
 	var final_pixel_position := final_cell_position * Vector2(tile_size)
 
@@ -76,6 +80,8 @@ func animate(sprite: Sprite2D, target_cell: Vector2, tile_size: Vector2i) \
 
 		if tween.is_running():
 			await tween.finished
+
+	return step_name
 
 
 func _get_final_cell_position(target_cell: Vector2) -> Vector2:
