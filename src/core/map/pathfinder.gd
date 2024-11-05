f97939494ce2c7e0416eda449acc9d6565cd60b1
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-11-05 17:30:59 -0500

    Fix pathfinder cell blocking

--- src/core/map/pathfinder.gd
+++ src/core/map/pathfinder.gd
@@ -21,33 +21,20 @@ func _init(map_region: Rect2i) -> void:
 func init_grid_for_actor_size(actor_size: Vector2i) -> void:
 	if (actor_size != Vector2i.ONE) and not _grids.has(actor_size):
 		var new_grid := _create_grid()
-		for x in range(_map_region.position.x, _map_region.end.x):
-			for y in range(_map_region.position.y, _map_region.end.y):
-				var cell := Vector2i(x, y)
-				if _base_grid.is_point_solid(cell):
-					Pathfinder._block_rect(
-							Rect2i(cell, Vector2i.ONE), new_grid, actor_size)
 		_grids[actor_size] = new_grid
+		_update_other_grids()
 
 
 ## Enables or disables a single cell for pathfinding.
 func set_cell_solid(cell: Vector2i, solid: bool) -> void:
-	set_rect_solid(Rect2i(cell, Vector2i.ONE), solid)
+	_base_grid.set_point_solid(cell, solid)
+	_update_other_grids()
 
 
 ## Enables or disables a rectangle of cells for pathfinding.
 func set_rect_solid(rect: Rect2i, solid: bool) -> void:
 	_base_grid.fill_solid_region(rect, solid)
-	@warning_ignore("untyped_declaration")
-	for a in _grids.keys():
-		@warning_ignore("unsafe_cast")
-		var actor_size := a as Vector2i
-		@warning_ignore("unsafe_cast")
-		var other_grid := _grids[actor_size] as AStarGrid2D
-		if solid:
-			Pathfinder._block_rect(rect, other_grid, actor_size)
-		else:
-			_clear_rect(rect, other_grid, actor_size)
+	_update_other_grids()
 
 
 ## Finds a path that would move [param start_rect] to a position adjacent to
@@ -74,19 +61,6 @@ func find_path_between_rects(start_rect: Rect2i, end_rect: Rect2i) \
 	return result
 
 
-static func _rect_to_update(rect: Rect2i, actor_size: Vector2i) -> Rect2i:
-	return Rect2i(
-		rect.position - actor_size + Vector2i.ONE,
-		rect.size + actor_size - Vector2i.ONE
-	)
-
-
-static func _block_rect(rect: Rect2i, grid: AStarGrid2D, actor_size: Vector2i) \
-		-> void:
-	var rect_to_update := Pathfinder._rect_to_update(rect, actor_size)
-	grid.fill_solid_region(rect_to_update, true)
-
-
 static func _end_cells(end_rect: Rect2i, actor_size: Vector2i,
 		grid: AStarGrid2D) -> Array[Vector2i]:
 	var result: Array[Vector2i] = []
@@ -100,22 +74,38 @@ static func _end_cells(end_rect: Rect2i, actor_size: Vector2i,
 	return result
 
 
-func _clear_rect(rect: Rect2i, grid: AStarGrid2D, actor_size: Vector2i) -> void:
-	var rect_to_update := Pathfinder._rect_to_update(rect, actor_size)
+func _update_other_grids() -> void:
+	var region := _base_grid.region
 
-	grid.fill_solid_region(rect_to_update, false)
+	@warning_ignore("untyped_declaration")
+	for k in _grids.keys():
+		@warning_ignore("unsafe_cast")
+		var actor_size := k as Vector2i
+		@warning_ignore("unsafe_cast")
+		var grid := _grids[actor_size] as AStarGrid2D
 
-	var still_blocked: Array[Vector2i] = []
-	for x in range(rect_to_update.position.x, rect_to_update.end.x):
-		for y in range(rect_to_update.position.y, rect_to_update.end.y):
-			var cell := Vector2i(x, y)
-			if _base_grid.is_in_boundsv(cell) \
-					and _base_grid.is_point_solid(cell):
-				still_blocked.append(cell)
-
-	for cell in still_blocked:
-		Pathfinder._block_rect(
-				Rect2i(cell, Vector2i.ONE), grid, actor_size)
+		grid.clear()
+		grid.region = region
+		grid.update()
+		for x in range(region.position.x, region.end.x):
+			for y in range(region.position.y, region.end.y):
+				var cell := Vector2i(x, y)
+				if _base_grid.is_point_solid(cell):
+					var block_rect := Rect2i(
+						cell - actor_size + Vector2i.ONE,
+						actor_size
+					)
+					grid.fill_solid_region(block_rect, true)
+		var bottom_border := Rect2i(
+			Vector2i(region.position.x, region.end.y - actor_size.y + 1),
+			Vector2i(region.size.x, actor_size.y - 1)
+		)
+		var right_border := Rect2i(
+			Vector2i(region.end.x - actor_size.x + 1, region.position.y),
+			Vector2i(actor_size.x - 1, region.size.y)
+		)
+		grid.fill_solid_region(bottom_border, true)
+		grid.fill_solid_region(right_border, true)
 
 
 func _create_grid() -> AStarGrid2D:
@@ -157,4 +147,6 @@ func _set_rect_solid_status(status: Dictionary) -> void:
 		var cell := k as Vector2i
 		@warning_ignore("unsafe_cast")
 		var is_solid := status[cell] as bool
-		set_cell_solid(cell, is_solid)
+		_base_grid.set_point_solid(cell, is_solid)
+
+	_update_other_grids()
