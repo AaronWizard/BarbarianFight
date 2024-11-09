18ec64839c75d070c5c57e9a71654016949e2dbd
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-11-08 23:36:36 -0500

    Ability effect template

---
+++ src/editor/script_templates/AbilityEffect/ability_effect_template.gd
@@ -0,0 +1,10 @@
+# meta-name: Ability Effect
+# meta-description: An ability effect.
+# meta-default: true
+
+class_name _CLASS_
+extends AbilityEffect
+
+
+func apply(_target: Vector2i, _source: Rect2i, _source_actor: Actor) -> void:
+	pass
