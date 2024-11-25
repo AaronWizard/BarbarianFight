83ce02eed38bb6f3c40c412777e9d29dfbe984eb
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-11-25 01:19:35 -0500

    Tweaked MapTargetRange

--- src/core/scenes/game/ui/target_display/map_target_range.gd
+++ src/core/scenes/game/ui/target_display/map_target_range.gd
@@ -2,7 +2,7 @@ class_name MapTargetRange
 extends Node2D
 
 @onready var _target_range_layer := $TargetRange as TileMapLayer
-@onready var _valid_targets_layer := $ValidTargets as TileMapLayer
+@onready var _selectable_targets_layer := $SelectableTargets as TileMapLayer
 
 const _TARGET_RANGE_TERRAIN_SET := 0
 const _TARGET_RANGE_TERRAIN := 0
@@ -15,12 +15,12 @@ func set_target_range(target_range: Array[Vector2i],
 		selectable_cells: Array[Vector2i]) -> void:
 	clear()
 	_set_target_range(target_range)
-	_set_valid_cells(selectable_cells)
+	_set_selectable_targets(selectable_cells)
 
 
 func clear() -> void:
 	_target_range_layer.clear()
-	_valid_targets_layer.clear()
+	_selectable_targets_layer.clear()
 
 
 func _set_target_range(target_range: Array[Vector2i]) -> void:
@@ -28,9 +28,9 @@ func _set_target_range(target_range: Array[Vector2i]) -> void:
 			target_range, _TARGET_RANGE_TERRAIN_SET, _TARGET_RANGE_TERRAIN)
 
 
-func _set_valid_cells(cells: Array[Vector2i]) -> void:
+func _set_selectable_targets(cells: Array[Vector2i]) -> void:
 	for cell in cells:
-		_valid_targets_layer.set_cell(
+		_selectable_targets_layer.set_cell(
 			cell,
 			_SELECTABLE_TARGET_CELL_SOURCE_ID,
 			_SELECTABLE_TARGET_CELL_ALTAS_COORDS
