32983bcf923841f054ad2369a56c382b1311703e
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-11-25 09:59:33 -0500

    Renamed some things again

--- src/core/scenes/game/ui/target_display/map_target_range.gd
+++ src/core/scenes/game/ui/target_display/map_target_range.gd
@@ -2,21 +2,21 @@ class_name MapTargetRange
 extends Node2D
 
 @onready var _target_range_layer := $TargetRange as TileMapLayer
-@onready var _selectable_targets_layer := $SelectableTargets as TileMapLayer
+@onready var _valid_targets_layer := $ValidTargets as TileMapLayer
 @onready var _aoe_layer := $AOE as TileMapLayer
 
 const _TARGET_RANGE_TERRAIN_SET := 0
 const _TARGET_RANGE_TERRAIN := 0
 
-const _SELECTABLE_TARGET_CELL_SOURCE_ID := 1
-const _SELECTABLE_TARGET_CELL_ALTAS_COORDS := Vector2i(0, 0)
+const _VALID_TARGET_CELL_SOURCE_ID := 1
+const _VALID_TARGET_CELL_ALTAS_COORDS := Vector2i(0, 0)
 
 
 func set_target_range(target_range: Array[Vector2i],
-		selectable_cells: Array[Vector2i]) -> void:
+		valid_targets: Array[Vector2i]) -> void:
 	clear()
 	_set_target_range(target_range)
-	_set_selectable_targets(selectable_cells)
+	_set_valid_targets(valid_targets)
 
 
 func set_aoe(_aoe: Array[Vector2i]) -> void:
@@ -25,7 +25,7 @@ func set_aoe(_aoe: Array[Vector2i]) -> void:
 
 func clear() -> void:
 	_target_range_layer.clear()
-	_selectable_targets_layer.clear()
+	_valid_targets_layer.clear()
 	_aoe_layer.clear()
 
 
@@ -34,10 +34,10 @@ func _set_target_range(target_range: Array[Vector2i]) -> void:
 			target_range, _TARGET_RANGE_TERRAIN_SET, _TARGET_RANGE_TERRAIN)
 
 
-func _set_selectable_targets(cells: Array[Vector2i]) -> void:
-	for cell in cells:
-		_selectable_targets_layer.set_cell(
+func _set_valid_targets(valid_targets: Array[Vector2i]) -> void:
+	for cell in valid_targets:
+		_valid_targets_layer.set_cell(
 			cell,
-			_SELECTABLE_TARGET_CELL_SOURCE_ID,
-			_SELECTABLE_TARGET_CELL_ALTAS_COORDS
+			_VALID_TARGET_CELL_SOURCE_ID,
+			_VALID_TARGET_CELL_ALTAS_COORDS
 		)
