ff40d150aeb849a0832e6707dd1aca372e49a58d
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-05 23:51:54 -0500

    Put ability menu node under target state node

--- src/core/scenes/game/states/player_select_ability_state.gd
+++ src/core/scenes/game/states/player_select_ability_state.gd
@@ -8,10 +8,9 @@ extends State
 ## The state for standard player movement and attacks.
 @export var movement_state: PlayerMovementState
 
-## The ability menu.
-@export var ability_menu: AbilityMenu
-
 var _player: Actor
+
+@onready var _ability_menu: AbilityMenu = $AbilityMenu
 
 
 func enter(data := {}) -> void:
@@ -24,23 +23,23 @@ func enter(data := {}) -> void:
 	for a in _player.abilities:
 		icons.append(a.icon)
 
-	ability_menu.position = _player.position + (
+	_ability_menu.position = _player.position + (
 		Vector2(_player.tile_size * _player.cell_size)
 		* Vector2(0.5, -0.5)
 	)
-	ability_menu.set_icons(icons)
+	_ability_menu.set_icons(icons)
 	@warning_ignore("return_value_discarded")
-	ability_menu.ability_clicked.connect(_ability_selected)
-	ability_menu.open()
+	_ability_menu.ability_clicked.connect(_ability_selected)
+	_ability_menu.open()
 
 
 func exit() -> void:
 	_player.map.mouse_clicked.disconnect(_map_clicked)
-	ability_menu.ability_clicked.disconnect(_ability_selected)
+	_ability_menu.ability_clicked.disconnect(_ability_selected)
 
 
 func _ability_selected(index: int) -> void:
-	await ability_menu.close()
+	await _ability_menu.close()
 
 	var ability := _player.abilities[index]
 	var targeting_data := ability.get_target_data(_player)
@@ -59,5 +58,5 @@ func _ability_selected(index: int) -> void:
 
 func _map_clicked(cell: Vector2i) -> void:
 	if cell in _player.covered_cells:
-		await ability_menu.close()
+		await _ability_menu.close()
 		request_state_change(movement_state, { player = _player })
